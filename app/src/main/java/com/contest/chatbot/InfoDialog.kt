93776bde8d4fe405492b956bbafbc0f34cf3acbd
Dohwan Kim <87230563+ehghks021203@users.noreply.github.com>
2024-06-05 18:04:30 +0900

    2024.06.05. Update InfoDialog.kt

--- app/src/main/java/com/contest/chatbot/InfoDialog.kt
+++ app/src/main/java/com/contest/chatbot/InfoDialog.kt
@@ -2,8 +2,15 @@ package com.contest.chatbot
 
 import android.app.Dialog
 import android.content.Context
+import android.content.Intent
+import android.graphics.Paint
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageButton
+
 
 class InfoDialog(context: Context) : Dialog(context) {
 
@@ -13,8 +20,8 @@ class InfoDialog(context: Context) : Dialog(context) {
 
         // Dialog를 전체 화면으로 설정
         window?.setLayout(
-            android.view.ViewGroup.LayoutParams.WRAP_CONTENT,
-            android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
         )
 
         val dialogButton = findViewById<ImageButton>(R.id.closeBtn)
@@ -22,5 +29,15 @@ class InfoDialog(context: Context) : Dialog(context) {
         dialogButton.setOnClickListener {
             dismiss()
         }
+
+        val termsButton = findViewById<Button>(R.id.termsBtn)
+        termsButton.paintFlags = termsButton.paintFlags or Paint.UNDERLINE_TEXT_FLAG
+
+        termsButton.setOnClickListener {
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.naver.com"))
+            this.context.startActivity(intent)
+        }
+
+
     }
 }
